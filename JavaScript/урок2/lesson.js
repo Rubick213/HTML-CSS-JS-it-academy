46c34e09af6e51ec3c5e0eb3aa1958679b83a744
Rubick213 <school30.96@mail.ru>
2024-04-02 21:32:50 +0300

    for_while

---
+++ JavaScript/урок2/lesson.js
@@ -0,0 +1,113 @@
+// let number = parseFloat(prompt('Введите дюбое положительное число'))
+
+// let test
+// let user = 15
+// if (number == null || number == "") {
+//     console.log('вы ничего не ввели');
+// } else if (!isFinite(number)) {
+//     console.log('вы ввели не число');
+// } else if ( number < 0) {
+//     console.log('вы ввели отрицательное число ');
+// } else if ( number > user) {
+//     console.log('ваше число больше');
+// } else if (number < user) {
+//     console.log('ваше число меньше ');
+// } else {
+//     console.log('ПОЗДРОВЛЯЕМ вы угадали число');
+// }
+
+// let apple = parseFloat(prompt('введите число от 0 до 5'))
+// switch(apple) {
+//     case 0 :
+//         console.log('0 яблоко');
+//         break;
+//     case 1 :
+//         console.log('1 яблоко');
+//         break;
+//     case 2 : 
+//     console.log('2 яблоко');
+//         break;
+//     case 3 :
+//         console.log('3 яблоко');
+//         break;
+//     case 4 : 
+//         console.log('4 яблоко');
+//         break;
+//     case 5 : 
+//         console.log('5 яблоко');
+//         break;
+//     default :
+//     console.log('error');
+// }
+
+// let year = prompt('Введите месяц рождения')
+
+// switch (year) {
+//     case 'январь':
+//     case 'февраль': 
+//     case 'декабрь':
+//         console.log('вы родились зимой'); 
+//         break;
+//     case 'март':
+//     case 'апрель': 
+//     case 'май':
+//         console.log('вы родились весной'); 
+//         break;
+//     case 'июнь':
+//     case 'июль': 
+//     case 'август':
+//         console.log('вы родились летом'); 
+//         break;
+//     case 'сентябрь':
+//     case 'октябрь': 
+//     case 'ноябрь':
+//         console.log('вы родились осенью'); 
+//         break;
+
+//     default:
+//         console.log('error');
+//         break;
+// }
+
+
+// let calc = 0
+// while (calc <= 100) {
+//     console.log(calc);
+//     calc+=10
+// }
+// let number = 10;
+// do {
+//    let prom = prompt('угадай число от 1 до 20')
+//     if (number == prom) {
+//         console.log('good');
+//         break
+//     }
+// } while (true)
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${i} * ${6} = ${i * 6}`);
+// }
+
+// let arr = []
+
+// for (let i = 0; i < 10; i++) {
+//     arr.push([])
+//     for (let k = 1; k < 10; k++) {
+//         arr[i].push(`${i+1} * ${k} = ${(i+1) * k}`)
+        
+//     }
+    
+// }
+// console.table(arr)
+// 7 17 29 78
+
+// for (let i = 1; i <= 100; i++) {
+//     if ( i === 7 || i === 17 || i === 29 || i === 78 ){
+//         continue
+//     } else {
+//         console.log(i);
+//     }
+// };
+
+// введите количетво строк 
+// введите кол столбцов
